f5a1769e9cd7c55d6a79b12164116951b55c5a9f
plastikSun <marcom2k01@gmail.com>
2023-11-04 19:00:31 +0100

    add: axios API call

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,11 @@ export default {
 
   components: { ProjectsList },
 
-  created() {},
+  created() {
+    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+      console.log(response.data);
+    });
+  },
 };
 </script>
 
