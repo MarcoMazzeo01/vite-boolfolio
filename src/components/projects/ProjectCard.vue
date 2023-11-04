1023900a245dc1c41dcf6811eafffe54d84b116d
plastikSun <marcom2k01@gmail.com>
2023-11-04 20:58:39 +0100

    add: project objec prop, passing data to card template to display API call response

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,14 +1,17 @@
-<script></script>
+<script>
+export default {
+  props: { project: Object },
+};
+</script>
 
 <template>
-  <div class="col">
-    <div class="card" style="width: 18rem">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">test</p>
-        <!-- <a href="#" class="card-link">Card link</a>
+  <div class="card h-100" style="width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">{{ project.title }}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.slug }}</h6>
+      <p class="card-text">{{ project.description }}</p>
+      <!-- <a href="#" class="card-link">Card link</a>
       <a href="#" class="card-link">Another link</a> -->
-      </div>
     </div>
   </div>
 </template>
