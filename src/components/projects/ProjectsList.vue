3349d2ff295896c3f1de72dd88a2455cf4488197
plastikSun <marcom2k01@gmail.com>
2023-11-04 20:57:22 +0100

    add: Projects array prop, passing project object as prop to ProjectCard component

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,7 +1,17 @@
 <script>
+import ProjectCard from "./ProjectCard.vue";
+
 export default {
   data() {
     return {};
+  },
+
+  components: {
+    ProjectCard,
+  },
+
+  props: {
+    projects: Array,
   },
 };
 </script>
@@ -9,6 +19,12 @@ export default {
 <template>
   <div class="container">
     <h2>Progetti</h2>
+
+    <div class="row row-cols-3 g-3">
+      <div class="col" v-for="project in projects">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
   </div>
 </template>
 
