1f452b7345d4bc0b3f01850926f0a639a994fd2f
plastikSun <marcom2k01@gmail.com>
2023-11-04 17:50:15 +0100

    create: projects list component

---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h2>Progetti</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
