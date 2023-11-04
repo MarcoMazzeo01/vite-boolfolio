ef6227ce79081ec247983e2c854e1ff2cd159915
plastikSun <marcom2k01@gmail.com>
2023-11-04 20:43:57 +0100

    add: card template

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <div class="col">
+    <div class="card" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">test</p>
+        <!-- <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
