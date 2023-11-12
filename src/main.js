679b120dc64cfab4349cad4d956fb07c1507ef60
plastikSun <marcom2k01@gmail.com>
2023-11-12 20:58:48 +0100

    fix: refactored router and moved into its own file

--- src/main.js
+++ src/main.js
@@ -1,25 +1,9 @@
 import { createApp } from "vue";
 import "./assets/scss/style.scss";
+import { router } from "./router.js";
 import * as bootstrap from "bootstrap";
 import App from "./App.vue";
 
-import { createRouter, createWebHistory } from "vue-router";
-import AppHome from "./pages/AppHome.vue";
-import ProjectList from "./pages/ProjectList.vue";
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: AppHome,
-    },
-    {
-      path: "/projects",
-      name: "projects",
-      component: ProjectList,
-    },
-  ],
-});
-
-createApp(App).use(router).mount("#app");
+const newApp = createApp(App);
+newApp.use(router);
+newApp.mount("#app");
