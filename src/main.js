e26c679a2623d4fc825ce8732579d348997ebca4
plastikSun <marcom2k01@gmail.com>
2023-11-12 20:45:34 +0100

    milestone 1

--- src/main.js
+++ src/main.js
@@ -3,4 +3,23 @@ import "./assets/scss/style.scss";
 import * as bootstrap from "bootstrap";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { createRouter, createWebHistory } from "vue-router";
+import AppHome from "./pages/AppHome.vue";
+import ProjectList from "./pages/ProjectList.vue";
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: AppHome,
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: ProjectList,
+    },
+  ],
+});
+
+createApp(App).use(router).mount("#app");
